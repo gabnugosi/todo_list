f88e983e75acaf29cc58824d9a1cd6950942c113
Gabriel Silva <gnrgs@outlook.com>
2024-02-21 17:04:56 -0300

    Teste de customização no campo de texto

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import "dart:developer";
+//import "dart:developer";
 
 import "package:flutter/material.dart";
 import "package:todo_list/pages/todo_list_pages.dart";
--- lib/pages/todo_list_pages.dart
+++ lib/pages/todo_list_pages.dart
@@ -1,11 +1,30 @@
-
 import 'package:flutter/material.dart';
 
-class TodoListPage extends StatelessWidget { //pode começar com stateles e se for necessário ir para stateful
+class TodoListPage extends StatelessWidget {
+  //pode começar com stateles e se for necessário ir para stateful
   const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red,);
+    return const Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            decoration: InputDecoration(
+              labelText: 'E-mail',
+              hintText: 'exemplo@exemplo.com',
+              //border: InputBorder.none,
+              errorText: null,
+              prefixText: 'R\$ ',
+              suffixText: 'cm',
+              labelStyle: TextStyle(fontSize: 40),
+            ),
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(fontWeight: FontWeight.w700, color: Colors.purple),
+          ),
+        ),
+      ),
+    );
   }
 }
