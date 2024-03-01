6576819c5a0934b0387f5e5f71d2cc62d283bd72
Gabriel Silva <gnrgs@outlook.com>
2024-03-01 13:22:25 -0300

    Aula 111 ler a lista de tarefas

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,14 +1,16 @@
-class Todo{
+class Todo {
   Todo({required this.title, required this.dateTime});
-  
+  Todo.fromJson(Map<String, dynamic> json)
+      : title = json['title'],
+        dateTime = DateTime.parse(json['dateTime']);
+
   String title;
   DateTime dateTime;
 
-  Map<String, dynamic> toJson(){
+  Map<String, dynamic> toJson() {
     return {
       'title': title,
       'dateTime': dateTime.toIso8601String(),
     };
   }
-
 }
--- lib/repositories/todo_repository.dart
+++ lib/repositories/todo_repository.dart
@@ -4,13 +4,18 @@ import 'dart:developer';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_list/models/todo.dart';
 
-class TodoRepository{
-  TodoRepository(){
-    SharedPreferences.getInstance().then((value) => sharedPreferences = value);
-  }
-  late SharedPreferences sharedPreferences;
+const todoListKey = 'todo_list';
 
-  void saveTodoList(List<Todo> todos){
+class TodoRepository {
+  late SharedPreferences sharedPreferences;
+  Future<List<Todo>> getTodoList() async {
+    sharedPreferences = await SharedPreferences.getInstance();
+    final String jsonString = sharedPreferences.getString(todoListKey) ?? '[]';
+    final List jsonDecoded = json.decode(jsonString) as List;
+    return jsonDecoded.map((e) => Todo.fromJson(e)).toList();
+  }
+
+  void saveTodoList(List<Todo> todos) {
     final jsonString = json.encode(todos);
     sharedPreferences.setString('todo_list', jsonString);
     log(jsonString);
